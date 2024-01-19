e5d8f908ee3dfd7d538c67d33d0dc82b11beefaa
Shivpalsingh15 <singhshivpal.coder@gmail.com>
2024-01-19 16:00:58 +0530

    complete all navbar

--- E-commerce.js
+++ E-commerce.js
@@ -1,13 +1,15 @@
+var mainData= []
 getData();
 async function getData(){
     let data = await fetch("https://fakestoreapi.com/products");
-    let responce = await data.json()
-    console.log('responce:', responce)
-    mapping(responce)
+     mainData = await data.json()
+    console.log('mainData:', mainData)
+    mapping(mainData)
 }
 
 function mapping(data){
     let container = document.getElementById('container')
+    container.innerHTML = ''
     // bhai waha class use kiya hai tune id se get kr rh hai islye 
     data.forEach(element => {
         
@@ -24,9 +26,16 @@ function mapping(data){
         let image = document.createElement('img')
         image.src = element.image
 
+        let rating = document.createElement('span')
+        rating.innerText = `Products rating ${element.rating.rate}`;
+
         let price = document.createElement('p')
         price.innerText = `Price: ${element.price}`;
         price.style.fontWeight = "bold";
+
+
+        let description = document.createElement('h4')
+        description.innerText = `Products description ${element.description}`;
 
          price.addEventListener("mouseover", () => {
            price.style.fontSize = "20px";
@@ -41,8 +50,54 @@ function mapping(data){
         mydiv.appendChild(title)
         mydiv.appendChild(image)
         mydiv.appendChild(price)
+        mydiv.appendChild(rating)
+        mydiv.appendChild(description);
 
         container.appendChild(mydiv)
     });
     
+}
+
+let category = document.getElementById("Category");
+category.addEventListener("change", getCategory);
+
+
+
+async function getCategory() {
+let category = document.getElementById("Category");
+console.log('category:', category.value)
+
+let newData = mainData.filter((el)=> el.category == category.value)
+mapping(newData);
+// newData.sort((a, b) => a.price - b.price);
+}
+  
+  
+  let price = document.getElementById('Price')
+  price.addEventListener("click", getPrice);
+
+
+  async function getPrice() {
+  let sortedData = [...mainData]; // Create a copy of the main data array
+  sortedData.sort((a, b) => a.price - b.price); // Sort the array based on price in ascending order
+  mapping(sortedData);
+}
+  
+
+let Rating = document.getElementById("Rating");
+Rating.addEventListener("click", getRating);
+
+
+async function getRating(){
+  let sortRating = [...mainData]
+  sortRating.sort((a, b) => a.rating.rate - b.rating.rate);
+  mapping(sortRating);
+
+
+  // let rate = document.getElementById('Rating')
+  // rate.addEventListener('click', )
+
+  // async function getRate(){
+  //   let sortRate = [...mainData]
+  // }
 }
---
+++ demo.js
@@ -0,0 +1,13 @@
+let value = "indore";
+let ar = [
+  { name: "nil", place: "indore" },
+  { name: "nil", place: "indore" },
+  { name: "nil", place: "lohari" },
+  { name: "nil", place: "pithampur" },
+];
+
+let data = ar.filter((el) => el.place === value);
+console.log("data:", data);
+
+let sorting = [15, 26, 41, 25, 32]
+console.log('sorting:', sorting.sort())
